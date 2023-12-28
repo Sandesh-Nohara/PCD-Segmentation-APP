39465268f6413f46ec1a8cb215156f3ea125289a
Sandesh-Nohara <122865900+Sandesh-Nohara@users.noreply.github.com>
2023-12-28 10:45:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -300,8 +300,8 @@ def visualize2():
 #    return render_template('visualization.html')
 
 
-# if __name__ == '__main__':  
-#    app.run(host='0.0.0.0', port=8080)  
+if __name__ == '__main__':  
+   app.run(host='0.0.0.0', port=8080)  
       
 # if __name__ == '__main__':  
 #   app.run()#debug = True)
